451fad8f2c4151970e3b24a041c590e59a72e7eb
Nishtha Sharma <nishthapinjore@gmail.com>
2025-07-24 12:36:30 +0530

    created the api (works well with images locally)

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import random
+
+
+app = FastAPI()
+
+images = "images"
+
+image_names = [f"image{i}.png" for i in range(1,6)]
+
+@app.get("/random-image")
+
+def get_random_image():
+    chosen = random.choice(image_names)
+    return FileResponse(f"{images}/{chosen}")
